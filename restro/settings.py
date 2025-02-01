59dfaed55ec8585d013ba2e509c6f42c5e62e03a
Prapti <prapti.tambe@gmail.com>
2025-02-01 01:34:46 +0000

    Add cards within carousel

--- restro/settings.py
+++ restro/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-!y*cu0k1xuma7fjh4v+-$2nye57fr7n!y-deonjb94@ycw=&2+
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
