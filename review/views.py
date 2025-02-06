b249410281e6de146e3047a885c5c73d3fcc5fd0
Prapti <prapti.tambe@gmail.com>
2025-02-06 07:23:47 +0000

    Add comment form and enhance comment submission functionality,update requirements for crispy forms

--- review/views.py
+++ review/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Restaurant,Ratings, Comments
 from django.db.models import Avg
+from django.contrib import messages
+from .models import Restaurant, Ratings, Comments
+from .forms import CommentForm
 
 # Create your views here.
 def home(request):
@@ -10,16 +12,31 @@ def home(request):
 def restro_detail(request, slug):
     restaurant = get_object_or_404(Restaurant, slug=slug)
     average_rating = Ratings.objects.filter(restro=restaurant).aggregate(Avg('rating'))['rating__avg']
-    comments = Comments.objects.filter(restro=restaurant)
+    comments = Comments.objects.filter(restro=restaurant).order_by("-created_at")
     ratings = Ratings.objects.filter(restro=restaurant)
-    comments = Comments.objects.all().order_by("-created_at")
     comment_count = Comments.objects.filter(is_approved=True).count()
+
+    if request.method == "POST":
+        print("Received a POST request")
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.restro = restaurant
+            comment.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Comment submitted and awaiting approval'
+            )
+
+    comment_form = CommentForm(data=request.POST)
+    print("About to render template")
 
     return render(request, 'review/restro_detail.html', {
         'restaurant': restaurant, 
         'average_rating': average_rating, 
         'comments': comments, 
         'ratings': ratings,
-        "comments": comments,
-        "comment_count": comment_count,
-        })
+        'comment_count': comment_count,
+        'comment_form': comment_form,
+    })
