f197a251ad71f785ee21d5879e3d0d624275b5e5
Prapti <prapti.tambe@gmail.com>
2025-02-06 05:17:05 +0000

    Enable debug mode for development and enhance comment display with count and styling adjustments

--- review/views.py
+++ review/views.py
@@ -12,9 +12,14 @@ def restro_detail(request, slug):
     average_rating = Ratings.objects.filter(restro=restaurant).aggregate(Avg('rating'))['rating__avg']
     comments = Comments.objects.filter(restro=restaurant)
     ratings = Ratings.objects.filter(restro=restaurant)
+    comments = Comments.objects.all().order_by("-created_at")
+    comment_count = Comments.objects.filter(is_approved=True).count()
+
     return render(request, 'review/restro_detail.html', {
         'restaurant': restaurant, 
         'average_rating': average_rating, 
         'comments': comments, 
-        'ratings': ratings
+        'ratings': ratings,
+        "comments": comments,
+        "comment_count": comment_count,
         })
