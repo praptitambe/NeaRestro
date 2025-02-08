e9ced4fe9d02b3fa968c49c657ea824dc7ba306b
Prapti <prapti.tambe@gmail.com>
2025-02-08 17:50:25 +0000

    Enhance comment editing functionality by adding rating selection and updating form action

--- static/js/comments.js
+++ static/js/comments.js
@@ -6,34 +6,27 @@ const deleteButtons = document.getElementsByClassName("btn-delete");
 const deleteConfirm = document.getElementById("deleteConfirm");
 const deleteModal = new bootstrap.Modal(document.getElementById("deleteModal"));
 
-// document.getElementById('write-comment-icon').addEventListener('click', function() {
-//     if (userIsAuthenticated === "false") {
-//         var loginModal = new bootstrap.Modal(document.getElementById('loginModal'));
-//         loginModal.show();
-//     } else {
-//         document.getElementById('comment-form-card').scrollIntoView({ behavior: 'smooth' });
-//     }
-// });
-
 document.querySelectorAll('.btn-edit').forEach(button => {
     button.addEventListener('click', function() {
         const commentTextValue = this.getAttribute('data-comment-text');
         const commentId = this.getAttribute('data-comment-id');
+        const commentRating = this.getAttribute('data-comment-rating');
         const commentTextarea = document.getElementById('id_comment');
+        const ratingSelect = document.getElementById('id_rating');
         const submitButton = document.getElementById('submitButton');
+        
         commentTextarea.value = commentTextValue;
+        ratingSelect.value = commentRating;
         submitButton.innerText = "Update";
         commentForm.action = `/restaurant/${restaurantSlug}/edit_comment/${commentId}/`;
         document.getElementById('comment-form-card').scrollIntoView({ behavior: 'smooth' });
     });
 });
 
-
 for (let button of deleteButtons) {
     button.addEventListener("click", (e) => {
         e.preventDefault();
         let commentId = e.target.getAttribute("data-comment_id");
-        console.log(commentId);
         deleteConfirm.href = `/restaurant/${restaurantSlug}/delete_comment/${commentId}/`;
         deleteModal.show();
     });
