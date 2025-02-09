fde31a6531d903f593f2ddb50f427dba523a8b5c
Prapti <prapti.tambe@gmail.com>
2025-02-09 04:14:10 +0000

    Improve search form and comment editing functionality by updating field labels and enhancing UI elements

--- review/forms.py
+++ review/forms.py
@@ -8,14 +8,14 @@ class CommentForm(forms.ModelForm):
         fields = ('comment','rating')
 
 class RestroSearchForm(forms.Form):
-    q = forms.CharField()
+    q = forms.CharField(label='', required=False, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Search for a restaurant by name or city'}))
     c = forms.ModelChoiceField(queryset=Cuisine.objects.all().order_by('cuisine_type'), empty_label='Filter by Cuisine', required=False)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['c'].label = ''
         self.fields['c'].required = False
-        self.fields['c'].label = 'Category'
+        self.fields['c'].label = 'Cuisine'
         self.fields['q'].label = 'Search For'
         self.fields['q'].widget.attrs.update(
             {'class': 'form-control menudd'})
